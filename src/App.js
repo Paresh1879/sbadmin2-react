055c6223df104fe11a60e18d0c5992a71137909a
Paresh N <natarajan.paresh@gmail.com>
2022-06-11 21:06:07 +0530

    sbadminpagereact

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Dashboard from "./Dashboard";
+import Navbar from "./Navbar";
+import "./sb-admin-2.min.css";
+
+function App() {
+  return (
+    <>
+      <div id="wrapper">
+        <Navbar />
+        <Dashboard />
+      </div>
+    </>
+  );
+}
+
+export default App;
